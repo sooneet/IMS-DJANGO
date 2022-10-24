9ca1420834935ab60f7674c8a01ae474f28fd9e5
sooneet <85282640+sooneet@users.noreply.github.com>
2022-10-24 12:25:19 +0545

    homepage

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
 
     path('', include('homepage.urls')),
     path('inventory/', include('inventory.urls')),
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class HomeView(View):
+    template_name = "home.html"
+    def get(self, request):        
+        context = {}
+        return render(request, self.template_name, context)
+
+
+
+# def home(request):
+#     return render(request,'home.html')
